7e9da1362af64626212e4511135de7c6541ea56c
Doctor <sokol767676@gmail.com>
2023-10-28 20:30:00 +0300

    + Регулярка для поиска необходимых пуктов файле чтобы взять температуру для всчитывание средней (пусть будет)

---
+++ Интерфес/TemperatureAndDephFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Интерфейсы_и_классы
+{
+    class TemperatureAndDephFile :TemperatureFile
+    {
+        public double AverageDepth;
+    }
+}
---
+++ Интерфес/TemperatureFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Интерфейсы_и_классы
+{
+    public class TemperatureFile
+    {
+        public FileInfo MainFile;
+        public double AverageTemperature;
+        public double DepthOfImmersion;
+        public TemperatureFile(FileInfo mainFile)
+        {
+            MainFile = mainFile;
+        }
+        public TemperatureFile() { }
+        public void CountAverageTemperature()
+        {
+            string pattern= @"[0-9]+/[0-9]+/[0-9]+\s[0-9]+:[0-9]+:[0-9]+.[0-9]+\s[0-9]+.[0-9]+\b";
+            List<string> MatchesList = new List<string>();
+            using (StreamReader sr = new StreamReader(MainFile.FullName))
+            {
+                MatchesList.Clear();
+                MatchCollection Matches = null;
+                Regex regex = new Regex(pattern);
+                foreach (string SubString in sr.ReadToEnd().Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    Matches = regex.Matches(SubString);
+                    foreach (Match m in Matches)
+                    {
+                        MatchesList.Add(m.Value);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
