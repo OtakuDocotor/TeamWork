11ad5fdfb53df8d4c034cb2e11c20c84536dcf52
IlyaGusev <gin510383@gmail.com>
2023-11-17 16:33:33 +0300

    Подключил библиотеку

--- Граф интерфейс/MainWindow.xaml.cs
+++ Граф интерфейс/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,15 +15,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Интерфейсы_и_классы;
 
 namespace Граф_интерфейс
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    /// 
+
+
     public partial class MainWindow : Window
     {
-        FileInfo[] filesArray;
+        TemperatureFile[] ArrayOf_CSV_Files;
+        FileInfo[] ArrayOfAllFiles;
+        string patternOf_CSV_File = @"[0-9].[0-9]+\sm_(TR|TDR)_[0-9]+_[0-9]+_[0-9]+";
+
+        public static TemperatureFile[] Find(FileInfo[] AllFiles, string pattern)
+        {
+            List<TemperatureFile> findingFiles = new List<TemperatureFile>();
+            int i = 0;
+            MatchCollection Matches = null;
+            Regex r = new Regex(pattern);
+            foreach (FileInfo q in AllFiles)
+            {
+                Matches = r.Matches(q.FullName);
+                foreach (Match m in Matches)
+                {
+                   // findingFiles.Add();
+                }
+            }
+
+            return findingFiles.ToArray();
+        }
 
         public MainWindow()
         {
@@ -35,13 +60,16 @@ namespace Граф_интерфейс
             if (FBD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 DirectoryInfo directory = new DirectoryInfo(FBD.SelectedPath);
-                filesArray = directory.GetFiles();
+                ArrayOfAllFiles = directory.GetFiles();
             }
+
+            ArrayOf_CSV_Files = Find(ArrayOfAllFiles, patternOf_CSV_File);
+
         }
         private void ViewingFiles(object sender, RoutedEventArgs e)
         {
             MainTextBox.Text = "";
-            foreach (var file in filesArray)
+            foreach (var file in ArrayOf_CSV_Files)
             {
                 MainTextBox.Text += file + "\n";
             }
