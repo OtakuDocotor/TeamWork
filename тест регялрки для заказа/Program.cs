ba985c2a1e2ba6ffb0a0f011955dd1d7c3d586a4
Doctor <sokol767676@gmail.com>
2023-11-24 19:10:08 +0300

    Обрезка значений Beta

--- тест регялрки для заказа/Program.cs
+++ тест регялрки для заказа/Program.cs
@@ -12,7 +12,7 @@ namespace тест_регялрки_для_заказа
 
     class Program
     {
-        
+
         public static List<string> Work = new List<string>();
         static void Main(string[] args)
         {
@@ -23,20 +23,15 @@ namespace тест_регялрки_для_заказа
             string pattern3 = @"[0-9].[0-9]+\sm_(TR|TDR)_[0-9]+_[0-9]+_[0-9]+"; // паттерн для имён файлов не проверял (´｡• ω •｡`) G
             string[] Arrr;
             List<string> LS = new List<string>();
-            string [] s;
+            string[] s;
             double average = 0;
             double sum = 0;
             List<TRMesure> All_Mesures = new List<TRMesure>();
+            List<TRMesure> Cutted_Mesures = new List<TRMesure>();
             Console.WriteLine("Найденные данные:");
-            //using (StreamReader sr =new StreamReader("0.60 m_TR_077222_20230419_1447.csv"))
-            //{
-            //    s = sr.ReadToEnd().Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-            //    Console.WriteLine(s.Length);
-            //    Find(s, pattern2);
-            //}
             using (StreamReader sr = new StreamReader("0.60 m_TR_077222_20230419_1447.csv"))
             {
-   
+
                 string[] S = sr.ReadToEnd().Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
                 MatchCollection Matches = null;
                 Regex r = new Regex(@"[0-9]+/[0-9]+/[0-9]+\s[0-9]+:[0-9]+:[0-9]+.[0-9]+[;]+[0-9]+.[0-9]+\b");
@@ -47,15 +42,34 @@ namespace тест_регялрки_для_заказа
                     {
                         string[] SubS = m.Value.Split(new char[] { ' ', ';', '/', ':', '.' }, StringSplitOptions.RemoveEmptyEntries);
                         All_Mesures.Add(new TRMesure(new DateTime(int.Parse(SubS[2]), int.Parse(SubS[1]), int.Parse(SubS[0]), int.Parse(SubS[3]), int.Parse(SubS[4]), int.Parse(SubS[5])), double.Parse(SubS[7])));
+                        sum += All_Mesures.Last()._Temperature;
                     }
                 }
 
             }
+            Console.WriteLine("AVG: {0}", sum / All_Mesures.Count);
+            average = sum / All_Mesures.Count;
+            int n = All_Mesures.Count();
+            Console.WriteLine("n={0}", n);
             foreach (TRMesure a in All_Mesures)
             {
-                Console.WriteLine(a);
+                if (a._Temperature < average)
+                {
+                    Cutted_Mesures.Add(a);
+                }
             }
+            Cutted_Mesures.RemoveRange(0, 250);
+            n = Cutted_Mesures.Count();
+            Cutted_Mesures.RemoveRange(n - 1200, 1200);
+            sum = 0;
+            foreach (TRMesure a in Cutted_Mesures)
+            {
+                Console.WriteLine(a);
+                sum += a._Temperature;
+            }
+            Console.WriteLine(sum / Cutted_Mesures.Count);
             Console.ReadKey();
+
         }
         public static void Find(string[] s, string pattern)
         {
